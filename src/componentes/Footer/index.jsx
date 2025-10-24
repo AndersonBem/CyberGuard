6d790c8337478d7b015faa104fd09157d1f9f806
AndersonBem <126602982+AndersonBem@users.noreply.github.com>
2025-10-24 10:23:08 -0300

    Change class to className in Footer component

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -4,8 +4,8 @@ import House from './assets/house.svg';
 export default function Footer({ text, children }){
     return (
         <div>
-        <footer class="app-footer">
-            <button class="btn-footer-home">
+        <footer className="app-footer">
+            <button className="btn-footer-home">
                 <img src={House} alt='Logo da casa' />
             </button>
         </footer>
